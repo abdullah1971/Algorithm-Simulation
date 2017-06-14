fb66a531333f06879d9993a5af1b065b3251e867
abdullah1971 <abdullahmaruf71@gmail.com>
2017-06-15 05:34:47 +0600

    Initial State

---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+$(".visPlusLink").resizable({
+  handleSelector: ".splitter",
+  resizeHeight: false
+});
+
+$(".visualization").resizable({
+   handleSelector: ".splitter-horizontal-1",
+   resizeWidth: false
+ });
+
+ $(".input-section").resizable({
+   handleSelector: ".splitter-horizontal-2",
+   resizeWidth: false
+ });
+
+
+/*$(document).ready(function(){
+    $(".main-section").resizable({
+      handleSelector: ".splitter",
+      resizeHeight: false
+    });
+});*/
+
